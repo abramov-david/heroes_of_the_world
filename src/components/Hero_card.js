686ab915e7dc5cd2a7bcb88d1309f713b76a881f
david-abramov <sendtoabramov@gmail.com>
2022-09-09 00:05:52 +0300

    add card animation

--- src/components/Hero_card.js
+++ src/components/Hero_card.js
@@ -1,20 +1,20 @@
 import React from "react";
-import classes from "./Hero_card.module.css";
+import "./Hero_card.css";
 import dc_logo from "../assets/dc_logo_white.png";
 import marvel_logo from "../assets/marvel_logo.png";
 import batman_img from "../assets/main_page/index_6.jpg";
 
 export default function Hero_card() {
   return (
-    <div className={classes.card}>
-      <div className={classes.front__side}>
-        <div className={classes.front__side__background}></div>
-        <img src={batman_img} className={classes.hero} alt="" />
-        <img src={dc_logo} className={classes.logo} alt="" />
+    <div className="hero-card">
+      <div className="hero-card__front-side">
+        <div className="hero-card__front-side--background"></div>
+        <img src={batman_img} className="hero-card__hero" alt="" />
+        <img src={dc_logo} className="hero-card__front-side--logo" alt="" />
         <p>BATMAN</p>
       </div>
-      <div className={classes.back__side}>
-        <img src={batman_img} className={classes.hero} alt="" />
+      <div className="hero-card__back-side">
+        <img src={batman_img} className="hero-card__hero" alt="" />
         <p>click to open</p>
       </div>
     </div>
